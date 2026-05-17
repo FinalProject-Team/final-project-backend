ee94e1e6745d23389afa48a8976610d6428619ad
hager nady <hagernady22@gmail.com>
2026-05-17 20:30:14 +0300

    complete swagger docs

--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,3 +1,9 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Courses
+ *   description: Courses API
+ */
 import express from "express";
 
 import {
@@ -9,10 +15,97 @@ import {
 } from "../controllers/courses.controller.js";
 
 const router = express.Router();
-
+/**
+ * @swagger
+ * /api/courses:
+ *   get:
+ *     summary: Get all courses
+ *     tags: [Courses]
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 router.get("/", getCourses);
+/**
+ * @swagger
+ * /api/courses/{id}:
+ *   get:
+ *     summary: Get single course
+ *     tags: [Courses]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Success
+ *       404:
+ *         description: Not found
+ */
 router.get("/:id", getSingleCourse);
+/**
+ * @swagger
+ * /api/courses:
+ *   post:
+ *     summary: Create a course
+ *     tags: [Courses]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             example:
+ *               title: "React Basics"
+ *               price: 100
+ *     responses:
+ *       201:
+ *         description: Created
+ */
 router.post("/", createCourse);
+/**
+ * @swagger
+ * /api/courses/{id}:
+ *   put:
+ *     summary: Update course
+ *     tags: [Courses]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             example:
+ *               title: "Updated Course"
+ *               price: 200
+ *     responses:
+ *       200:
+ *         description: Updated
+ */
 router.put("/:id", updateCourse);
+/**
+ * @swagger
+ * /api/courses/{id}:
+ *   delete:
+ *     summary: Delete course
+ *     tags: [Courses]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Deleted
+ */
 router.delete("/:id", deleteCourse);
 export default router;
