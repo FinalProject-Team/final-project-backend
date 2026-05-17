c15cadcafecf9e6886448567501c1cb318c118d8
hager nady <hagernady22@gmail.com>
2026-05-17 18:33:28 +0300

    add get single course api

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -16,3 +16,19 @@ export const getCourses = async (req, res) => {
 
     }
 };
+
+export const getSingleCourse = async (req, res) => {
+    const { id } = req.params;
+
+    const { data, error } = await supabase
+        .from("courses")
+        .select("*")
+        .eq("id", id)
+        .single();
+
+    if (error) {
+        return res.status(500).json({ error: error.message });
+    }
+
+    res.json(data);
+};
--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import {
-    getCourses
+    getCourses,
+    getSingleCourse
 } from "../controllers/courses.controller.js";
 
 const router = express.Router();
 
 router.get("/", getCourses);
+router.get("/:id", getSingleCourse);
 
 export default router;
