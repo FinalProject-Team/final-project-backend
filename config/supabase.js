fce9c058d7dba2ca45b7c51fd29dee892ea22063
hager nady <hagernady22@gmail.com>
2026-05-23 10:48:46 +0300

    fix supabase realtime websocket issue

--- config/supabase.js
+++ config/supabase.js
@@ -1,16 +1,27 @@
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
+import ws from "ws";
 
 dotenv.config();
 
+const supabaseConfig = {
+    realtime: {
+        transport: ws
+    }
+};
+
+// public client
 const supabase = createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
+    process.env.SUPABASE_ANON_KEY,
+    supabaseConfig
 );
 
+// admin client
 export const supabaseAdmin = createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
+    process.env.SUPABASE_SERVICE_ROLE_KEY,
+    supabaseConfig
 );
 
 export default supabase;
