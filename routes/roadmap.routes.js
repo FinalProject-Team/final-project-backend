199c1e0a1d5b80290ba6a0828d18399d76709cc0
hager nady <hagernady22@gmail.com>
2026-05-23 11:21:55 +0300

    add roadmap to swagger

--- routes/roadmap.routes.js
+++ routes/roadmap.routes.js
@@ -8,6 +8,22 @@ import {
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/roadmap:
+ *   get:
+ *     summary: Get user roadmap
+ *     tags: [Roadmap]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Roadmap fetched successfully
+ *       401:
+ *         description: Unauthorized (missing or invalid token)
+ *       500:
+ *         description: Server error
+ */
 router.get(
     "/",
     protect,
