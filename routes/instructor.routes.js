62994db2c042213378c2ed95d433b4895d173c31
hager1nady <hajrnady24@gmail.com>
2026-05-28 17:03:10 +0300

    update instractor to connect with swagger

--- routes/instructor.routes.js
+++ routes/instructor.routes.js
@@ -6,6 +6,42 @@ import { getInstructorDashboard } from "../controllers/instructor.controller.js"
 const router = express.Router();
 
 
+/**
+ * @swagger
+ * tags:
+ *   name: Instructor
+ *   description: Instructor Dashboard APIs
+ */
+
+
+/**
+ * @swagger
+ * /api/instructor/dashboard:
+ *   get:
+ *     summary: Get instructor dashboard statistics
+ *     tags: [Instructor]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Dashboard data retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               example:
+ *                 totalCourses: 5
+ *                 totalLessons: 20
+ *                 totalStudents: 100
+ *
+ *       401:
+ *         description: Unauthorized
+ *
+ *       403:
+ *         description: Forbidden
+ */
+
+
 // ======================
 // Instructor Dashboard
 // ======================
