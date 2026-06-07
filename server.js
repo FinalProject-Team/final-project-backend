d889426f4575dc3251d5dad4912c4ff1010bff32
hager nady <hagernady22@gmail.com>
2026-06-07 18:39:28 +0300

    open cors to all ports

--- server.js
+++ server.js
@@ -36,16 +36,7 @@ app.set("trust proxy", 1);
 //     credentials: true
 // }));
 
-app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5179",
-        "https://final-project-backend-production-214a.up.railway.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 
