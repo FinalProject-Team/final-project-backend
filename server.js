5005797f7b5427838b621db3d4fe834630e8d8dd
hager nady <hagernady22@gmail.com>
2026-06-07 18:09:37 +0300

    update ports

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "http://localhost:5174",
+        "http://localhost:5179",
         "https://final-project-backend-production-214a.up.railway.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
