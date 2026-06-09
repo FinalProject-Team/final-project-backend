98a551c166178fd2b6f5fcf89a2e7b05e545eb2e
hager nady <hagernady22@gmail.com>
2026-06-09 12:41:51 +0300

    change

--- middlewares/authorize.middleware.js
+++ middlewares/authorize.middleware.js
@@ -1,6 +1,9 @@
 export const authorize = (...roles) => {
     return (req, res, next) => {
+        console.log("🔥 AUTHORIZE HITtttttttttttttttt");
         console.log("🔥 AUTHORIZE HIT");
+        console.log("ROLE:", req.profile?.role);
+        console.log("ALLOWED:", roles);
 
         if (!req.profile) {
             return res.status(403).json({ message: "No profile" });
